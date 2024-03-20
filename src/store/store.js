8b9f9b2094d690e4732ebbd4dae4c2e6011884f3
Skunk-XCP <skunk-xcp@hotmail.fr>
2024-03-20 09:32:02 +0100

    add Redux Persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,40 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+   FLUSH,
+   PAUSE,
+   PERSIST,
+   PURGE,
+   REGISTER,
+   REHYDRATE,
+   persistReducer,
+   persistStore,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import employeeReducer from "./reducers/reducer";
 
-const store = configureStore({
-   reducer: {
-      employees: employeeReducer,
-   },
+const persistConfig = {
+   key: "root", // Clé de base pour le stockage
+   storage, // Définit le mécanisme de stockage
+   whitelist: ["employees"],
+};
+
+const rootReducer = combineReducers({
+   employees: employeeReducer,
 });
 
-export default store;
+// Crée un reducer persistant qui enveloppe rootReducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Configure le store Redux avec le reducer persistant
+export const store = configureStore({
+   reducer: persistedReducer, // Utilise le reducer persistant comme reducer principal
+   middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+         serializableCheck: {
+            // Configuration spéciale pour éviter des erreurs de serialisation avec certaines actions de redux-persist
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+         },
+      }),
+});
+// Crée un persistor pour le store, nécessaire pour le rechargement persistant du state
+export const persistor = persistStore(store);
